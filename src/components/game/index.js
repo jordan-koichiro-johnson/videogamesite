6e799ec9841bb5ffd576c88c934b9ccc68e724b7
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-12-22 00:54:31 -0800

    working on ratings

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom"
+import star from "../../assets/star-svgrepo-com.svg"
+import './style.css'
 
 function Game() {
     const { game } = useParams()
     return (
-        <div>{game.split('_').join(' ')}</div>
+        <div>
+            {game.split('_').join(' ')}
+            <div className="reviewAvg">
+                <img href={star} className='star' />
+            </div>
+        </div>
     )
 }
 
